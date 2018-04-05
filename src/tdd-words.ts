9b37202c33a39532ca36f562547ee966e66f7c00
Wibodha Alkegama <w.alkegama@craneware.com>
2018-04-05 16:50:37 +0100

    some refactoring

--- src/tdd-words.ts
+++ src/tdd-words.ts
@@ -1,18 +1,19 @@
 export class TddWords {
 
     countWords(input: string) {
-        const validInput = input !== null && input !== '' && input.trim() !== '';
+        const validInput: boolean = input !== null && input !== '' && input.trim() !== '';
 
         if (validInput) {
 
             const normalizedStr = input.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-            const inputStringAsArray: string[] = normalizedStr.replace(/[^A-z0-9\.]+/g, ' ').split(' ').filter(Boolean);
+            const inputStringAsList: string[] = normalizedStr.replace(/[^A-z0-9\.]+/g, ' ').split(' ').filter(Boolean);
 
             let replaceCapsToSimple = (word): string => {
                 return word.replace(/[A-Z]/g, (match, offset, string) =>
                     (offset > 0 ? '' : '') + match.toLowerCase());
             }
-            let lowerCaseWordList = replaceCapsToSimple(inputStringAsArray.join(' ')).split(' ');
+
+            let lowerCaseWordList = replaceCapsToSimple(inputStringAsList.join(' ')).split(' ');
 
             let uniqueStringList = (): string[] => (
                 lowerCaseWordList.filter((value, index, array) => array.indexOf(value) === index)
