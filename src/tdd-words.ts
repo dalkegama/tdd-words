31891dc40b4b873332a3bd90ef8b9e64089385e2
Wibodha Alkegama <w.alkegama@craneware.com>
2018-04-06 09:00:17 +0100

    tidy up the code and few refactoring

--- src/tdd-words.ts
+++ src/tdd-words.ts
@@ -4,25 +4,30 @@ export class TddWords {
         const validInput: boolean = input !== null && input !== '' && input.trim() !== '';
 
         if (validInput) {
-
             const normalizedStr = input.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-            const inputStringAsList: string[] = normalizedStr.replace(/[^A-z0-9\.]+/g, ' ').split(' ').filter(Boolean);
+            const normalisedStringAsList: string[] = normalizedStr.replace(/[^A-z0-9\.]+/g, ' ').split(' ').filter(Boolean);
+            const lowerCaseStringList = this.replaceCapsToSimple(normalisedStringAsList.join(' ')).split(' ');
 
-            let replaceCapsToSimple = (word: string): string => {
-                return word.replace(/[A-Z]/g, (match: any, offset: any, string: string) =>
-                    (offset > 0 ? '' : '') + match.toLowerCase());
-            }
-
-            let lowerCaseWordList = replaceCapsToSimple(inputStringAsList.join(' ')).split(' ');
-
-            let uniqueStringList = (): string[] => (
-                lowerCaseWordList.filter((value, index, array) => array.indexOf(value) === index)
-            );
-
-            return uniqueStringList().length;
+            return this.uniqueStringList(lowerCaseStringList).length;
         }
         else {
             return 0;
         }
     }
+    private replaceCapsToSimple(word: string): string {
+        return word.replace(/[A-Z]/g, (match: any, offset: any, string: string) =>
+            (offset > 0 ? '' : '') + match.toLowerCase());
+    }
+
+    private uniqueStringList(lowerCaseStringList: any[]): string[] {
+        return lowerCaseStringList.filter((value, index, array) => array.indexOf(value) === index)
+    }
 }
+/*******************************************************************************************************
+ *  1. normalize()ing to NFD Unicode normal form decomposes combined graphemes into the
+ *     combination of simple ones. The è of Crème ends up expressed as e + ̀.
+ *  2. Using a regex character class to match the U+0300 → U+036F range,
+ *     it is now trivial to globally get rid of the diacritics,
+ *     which the Unicode standard conveniently groups as the Combining Diacritical Marks Unicode block.
+ *     https://stackoverflow.com/questions/990904/remove-accents-diacritics-in-a-string-in-javascript
+ *******************************************************************************************************/
